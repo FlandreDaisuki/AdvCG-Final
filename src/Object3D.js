87e17da31060554d2a20c77b62f8c1570fcb53ac
FlandreDaisuki <vbnm123c@gmail.com>
2015-06-15 23:35:13 +0800

    Sphere Triangle extend Object3D

--- src/Object3D.js
+++ src/Object3D.js
@@ -1,11 +1,42 @@
-Object3D = function () {
-    Object.defineProperty( this, 'id', { value: Object3DIdCount ++ } );
+Object3D = function() {
+    Object.defineProperty(this, 'id', { value: Object3DIdCount++ });
     this.name = '';
     this.type = 'Object3D';
 
-    this.parent = undefined;
     this.children = [];
     this.position = new Vector3();
 }
 
 Object3DIdCount = 0;
+
+// Sphere extend Object3D
+Sphere = function(center, radius) {
+    Object3D.call(this);
+    this.type = 'Sphere';
+
+    if (center instanceof(Vector3)) {
+        this.center = center;
+    } else {
+        this.center = new Vector3();
+    }
+
+    this.radius = (radius !== undefined) ? radius : 5;
+}
+
+Sphere.prototype = {
+    constructor: Sphere
+};
+
+// Triangle extend Object3D
+Triangle = function(a, b, c) {
+    Object3D.call(this);
+    this.type = 'Triangle';
+
+    this.a = (a !== undefined) ? a : new Vector3();
+    this.b = (b !== undefined) ? b : new Vector3();
+    this.c = (c !== undefined) ? c : new Vector3();
+}
+
+Triangle.prototype = {
+    constructor: Triangle
+};
