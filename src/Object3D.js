20b796630bc07e04d71e1c60140135bf34a1ecd6
FlandreDaisuki <vbnm123c@gmail.com>
2015-06-16 00:16:58 +0800

    Scene,Camera extend Object3D

--- src/Object3D.js
+++ src/Object3D.js
@@ -1,5 +1,5 @@
-Object3D = function() {
-    Object.defineProperty(this, 'id', { value: Object3DIdCount++ });
+Object3D = function () {
+    Object.defineProperty( this, 'id', { value: Object3DIdCount++ } );
     this.name = '';
     this.type = 'Object3D';
 
@@ -10,17 +10,17 @@ Object3D = function() {
 Object3DIdCount = 0;
 
 // Sphere extend Object3D
-Sphere = function(center, radius) {
-    Object3D.call(this);
+Sphere = function ( center, radius ) {
+    Object3D.call( this );
     this.type = 'Sphere';
 
-    if (center instanceof(Vector3)) {
+    if ( center instanceof( Vector3 ) ) {
         this.center = center;
     } else {
         this.center = new Vector3();
     }
 
-    this.radius = (radius !== undefined) ? radius : 5;
+    this.radius = ( radius !== undefined ) ? radius : 5;
 }
 
 Sphere.prototype = {
@@ -28,15 +28,44 @@ Sphere.prototype = {
 };
 
 // Triangle extend Object3D
-Triangle = function(a, b, c) {
-    Object3D.call(this);
+Triangle = function ( a, b, c ) {
+    Object3D.call( this );
     this.type = 'Triangle';
 
-    this.a = (a !== undefined) ? a : new Vector3();
-    this.b = (b !== undefined) ? b : new Vector3();
-    this.c = (c !== undefined) ? c : new Vector3();
+    this.a = ( a !== undefined ) ? a : new Vector3();
+    this.b = ( b !== undefined ) ? b : new Vector3();
+    this.c = ( c !== undefined ) ? c : new Vector3();
 }
 
 Triangle.prototype = {
     constructor: Triangle
 };
+
+// Scene extend Object3D
+Scene = function () {
+    Object3D.call( this );
+    this.type = 'Scene';
+}
+
+Scene.prototype = {
+    constructor: Scene
+};
+
+// Camera extend Object3D
+Camera = function ( fov ) {
+    Object3D.call( this );
+    this.type = 'Camera';
+
+    this.fov = ( fov !== undefined ) ? fov : 50; //degree
+}
+
+Camera.prototype = {
+    constructor: Camera,
+    fovDegree: function () {
+        return this.fov;
+    },
+    fovRad: function () {
+        return this.fov * Math.PI / 180;
+    }
+};
+
