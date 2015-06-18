3562b0ce0b2ce3e62ca550b288bc93c0947248e8
FlandreDaisuki <vbnm123c@gmail.com>
2015-06-18 09:55:51 +0800

    add method for Vector3

--- src/Vector3.js
+++ src/Vector3.js
@@ -1,5 +1,21 @@
-Vector3 = function(x, y, z) {
+Vector3 = function ( x, y, z ) {
     this.x = x || 0;
     this.y = y || 0;
     this.z = z || 0;
 };
+
+Vector3.prototype.constructor = Vector3;
+
+Vector3.prototype.subVectors = function ( a, b ) {
+    this.x = a.x - b.x;
+    this.y = a.y - b.y;
+    this.z = a.z - b.z;
+    return this;
+}
+
+Vector3.prototype.subScalar = function ( s ) {
+    this.x -= s;
+    this.y -= s;
+    this.z -= s;
+    return this;
+}
