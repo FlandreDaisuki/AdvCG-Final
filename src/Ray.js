186ac235fa0309fddb0efa716310c60ec66a087f
FlandreDaisuki <vbnm123c@gmail.com>
2015-06-19 17:05:40 +0800

    add intersect sphere method

--- src/Ray.js
+++ src/Ray.js
@@ -1,6 +1,6 @@
 Ray = function ( origin, direction ) {
-    this.origin = ( origin !== undefined ) ? origin : new THREE.Vector3();
-    this.direction = ( direction !== undefined ) ? direction.normalize() : new THREE.Vector3();
+    this.origin = ( origin !== undefined ) ? origin : new Vector3();
+    this.direction = ( direction !== undefined ) ? direction.normalize() : new Vector3();
 }
 
 Ray.prototype.constructor = Ray;
@@ -11,3 +11,65 @@ Ray.prototype.copy = function ( ray ) {
 
     return this;
 }
+
+Ray.prototype.at = function ( t, optionalTarget ) {
+    var result = optionalTarget || new Vector3();
+    return result.copy( this.direction ).multiplyScalar( t ).add( this.origin );
+}
+
+Ray.prototype.distanceToPoint = function () {
+
+    var v1 = new Vector3();
+
+    return function ( point ) {
+        var directionDistance = v1.subVectors( point, this.origin ).dot( this.direction );
+        // point behind the ray
+        if ( directionDistance < 0 ) {
+            return this.origin.distanceTo( point );
+        }
+
+        v1.copy( this.direction ).multiplyScalar( directionDistance ).add( this.origin );
+        return v1.distanceTo( point );
+    };
+}()
+
+Ray.prototype.isIntersectionSphere = function ( sphere ) {
+    return this.distanceToPoint( sphere.center ) <= sphere.radius;
+}
+
+Ray.prototype.intersectSphere = function () {
+    // from http://www.scratchapixel.com/lessons/3d-basic-lessons/lesson-7-intersecting-simple-shapes/ray-sphere-intersection/
+    var v1 = new Vector3();
+
+    return function ( sphere, optionalTarget ) {
+
+        v1.subVectors( sphere.center, this.origin );
+
+        var tca = v1.dot( this.direction );
+
+        var d2 = v1.dot( v1 ) - tca * tca;
+
+        var radius2 = sphere.radius * sphere.radius;
+
+        if ( d2 > radius2 ) return null;
+
+        var thc = Math.sqrt( radius2 - d2 );
+
+        // t0 = first intersect point - entrance on front of sphere
+        var t0 = tca - thc;
+
+        // t1 = second intersect point - exit point on back of sphere
+        var t1 = tca + thc;
+
+        // test to see if both t0 and t1 are behind the ray - if so, return null
+        if ( t0 < 0 && t1 < 0 ) return null;
+
+        // test to see if t0 is behind the ray:
+        // if it is, the ray is inside the sphere, so return the second exit point scaled by t1,
+        // in order to always return an intersect point that is in front of the ray.
+        if ( t0 < 0 ) return this.at( t1, optionalTarget );
+
+        // else t0 is in front of the ray, so return the first collision point scaled by t0 
+        return this.at( t0, optionalTarget );
+    }
+}()
--- src/Vector3.js
+++ src/Vector3.js
@@ -13,6 +13,34 @@ Vector3.prototype.copy = function ( v ) {
     return this;
 }
 
+Vector3.prototype.add = function ( v ) {
+    this.x += v.x;
+    this.y += v.y;
+    this.z += v.z;
+    return this;
+}
+
+Vector3.prototype.addScalar = function ( s ) {
+    this.x += s;
+    this.y += s;
+    this.z += s;
+    return this;
+}
+
+Vector3.prototype.addVectors = function ( a, b ) {
+    this.x = a.x + b.x;
+    this.y = a.y + b.y;
+    this.z = a.z + b.z;
+    return this;
+}
+
+Vector3.prototype.sub = function ( v ) {
+    this.x -= v.x;
+    this.y -= v.y;
+    this.z -= v.z;
+    return this;
+}
+
 Vector3.prototype.subVectors = function ( a, b ) {
     this.x = a.x - b.x;
     this.y = a.y - b.y;
@@ -24,6 +52,20 @@ Vector3.prototype.subScalar = function ( s ) {
     this.x -= s;
     this.y -= s;
     this.z -= s;
+    return this;
+}
+
+Vector3.prototype.multiplyScalar = function ( scalar ) {
+    this.x *= scalar;
+    this.y *= scalar;
+    this.z *= scalar;
+    return this;
+}
+
+Vector3.prototype.multiplyVectors = function ( a, b ) {
+    this.x = a.x * b.x;
+    this.y = a.y * b.y;
+    this.z = a.z * b.z;
     return this;
 }
 
@@ -41,10 +83,32 @@ Vector3.prototype.divideScalar = function ( scalar ) {
     return this;
 }
 
+Vector3.prototype.negate = function () {
+    this.x = -this.x;
+    this.y = -this.y;
+    this.z = -this.z;
+    return this;
+}
+
+Vector3.prototype.dot = function ( v ) {
+    return this.x * v.x + this.y * v.y + this.z * v.z;
+}
+
 Vector3.prototype.length = function () {
     return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
 }
 
 Vector3.prototype.normalize = function () {
     return this.divideScalar( this.length() );
+}
+
+Vector3.prototype.distanceTo = function ( v ) {
+    return Math.sqrt( this.distanceToSquared( v ) );
+}
+
+Vector3.prototype.distanceToSquared = function ( v ) {
+    var dx = this.x - v.x;
+    var dy = this.y - v.y;
+    var dz = this.z - v.z;
+    return dx * dx + dy * dy + dz * dz;
 }
